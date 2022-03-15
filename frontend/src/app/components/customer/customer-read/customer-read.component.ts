9f714ae303062441b60ac5d64cb79df2b9a4f520
Joao Victor <joaovictor.cogomes@gmail.com>
2022-03-15 11:44:55 -0300

    Adding pagination and row sorting with a datasource

--- frontend/src/app/components/customer/customer-read/customer-read.component.ts
+++ frontend/src/app/components/customer/customer-read/customer-read.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
 import { Customer } from '../customer.model';
 import { CustomerService } from '../customer.service';
+import { CustomerReadDataSource } from './customer-read-datasource';
 
 @Component({
   selector: 'app-customer-read',
   templateUrl: './customer-read.component.html',
   styleUrls: ['./customer-read.component.css']
 })
-export class CustomerReadComponent implements OnInit {
+export class CustomerReadComponent implements AfterViewInit {
 
-  customers: Customer[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<Customer>;
+  dataSource: CustomerReadDataSource;
+
   displayedColumns = ['id', 'name', 'age', 'phone', 'action'];
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService) {
+    this.dataSource = new CustomerReadDataSource();
+  }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.customerService.read().subscribe(customers => {
-      this.customers = customers;
+      this.dataSource.data = customers;
+
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.table.dataSource = this.dataSource;
     });
   }
 
